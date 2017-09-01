5d9f67ee446f818d76d7ccecaf9f68295338369c
Joe Meadway <joemeadway@gmail.com>
2017-09-01 17:32:06 +0800

    added tests, eslint

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1123.92).format('$0,0.00');
-debugger;
-console.log(`Pay ${courseValue} for this course`);
+
+console.log(`Pay ${courseValue} for this course`);  // eslint-disable-line no-console
